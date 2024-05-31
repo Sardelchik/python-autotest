55a0cef6aa89c5463a3ae9a3605a58d4b9f3ad48
Sardelchik <esenin1993@mail.ru>
2024-05-31 19:07:51 +0700

    requinments-added
    venv-initialization

--- example/conftest.py
+++ example/conftest.py
@@ -5,5 +5,5 @@ log_file = path.join(path.dirname(path.abspath(__file__)), 'logging.ini')
 logging.config.fileConfig(log_file)
 
 pytest_plugins = [
-    "src.fixtures"
+    "example.src.fixtures"
 ]
