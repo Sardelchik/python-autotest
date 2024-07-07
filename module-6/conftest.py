2cf31227019e4a0ee16994e4b183d9e40171bc0a
Sardelchik <esenin1993@mail.ru>
2024-07-07 14:16:36 +0700

    logging-added

--- module-6/conftest.py
+++ module-6/conftest.py
@@ -1,3 +1,22 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Настройка формата логирования
+log_format = "[%(asctime)s] %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format, datefmt='%Y-%m-%d %H:%M:%S,%f')
+
+# Создание обработчика для записи логов в файл с ротацией
+handler = RotatingFileHandler('request.log', maxBytes=5000000, backupCount=5)
+handler.setFormatter(logging.Formatter(log_format))
+
+# Получение экземпляра логгера и добавление обработчика
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+
+# Пример записи в лог
+logger.info('request: Request was sended')
+
+
 pytest_plugins=[
    "src.tests.browser"
 ]
